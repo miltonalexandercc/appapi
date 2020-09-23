9da3cc3266c3052179d5815ad4a03115bc4bb388
milton cely <miltoncely@wposs.com>
2020-09-23 14:35:32 -0500

    Primer commit, integracion de firebase.

---
+++ app/src/main/java/com/ufps/app_api/MainActivity.java
@@ -0,0 +1,26 @@
+package com.ufps.app_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("message", "Firebase integration.");
+        mFirebaseAnalytics.logEvent("Home", bundle);
+    }
+
+}
